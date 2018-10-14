3fe1162754d92b0665226b2595cab8c44d7ea2a7
StevenMHernandez <contact@stevenhdesigns.com>
2018-10-14 12:47:11 -0400

    assignment2: add crossvalidation, add plotted images

--- assignment2/svm.m
+++ assignment2/svm.m
@@ -1,6 +1,7 @@
 close all, format compact, clear all;
+tic;
 
-% rng(1,'v4normal')
+rng(1,'v4normal')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 
@@ -92,13 +93,19 @@ label_strings = strings(length(C0) * 2, 1);
 labels_i = 1;
 
 % store errors and accuracy values for plotting later
-err_polynomial = zeros(size(parameters_polynomial));
-err_guassian = zeros(size(parameters_guassian));
 accuracy_polynomial = zeros(size(parameters_polynomial));
 accuracy_guassian = zeros(size(parameters_guassian));
 
+C_best_polynomial = C0(1);
+C_best_guassian = C0(1);
+degree_best_polynomial = parameters_polynomial(1);
+sigma_best_guassian = parameters_guassian(1);
+
+lowest_polynomial_error = inf;
+lowest_guassian_error = inf;
+
 % Hold onto all figures for plotting accuracy and # of errors
-for i = 2:5
+for i = 2:3
     figure(i)
     hold on
 end
@@ -115,83 +122,100 @@ for kernel = 1:2 % 1: polynomial, 2: guassian % TODO: add guassian
 
         for p_i = 1:length(parameters)
             param = parameters(p_i);
+            
+            % 5-fold-crossvalidation
             numErr = 0;
+            indices = crossvalind('Kfold',Y,5);
+            for i = 1:5
+                ind_test = (indices == i);
+                ind_train = ~ind_test;
 
-            % Store all Y_pred (before applying sign()) so that we then use
-            % max() to determing the prediction for the model
-            Y_pred_all = zeros(l,length(unique(Y)'));
+                X_train = X(ind_train, :);
+                Y_train = Y(ind_train, :);
+                X_test = X(ind_test, :);
+                Y_test = Y(ind_test, :);
 
-            % Create a classifier for each class
-            for c = unique(Y)'
-                % Make true-classes: +1 and false-classes: -1
-                Yc = ((Y == c) * 2) - 1; 
+                % Store all Y_pred (before applying sign()) so that we then use
+                % max() to determing the prediction for the model
+                [l_test, ~] = size(X_test);
+                Y_pred_all = zeros(l_test,length(unique(Y_test)'));
+                [l, dim] = size(X_train);
 
-                if kernel == 1 % polynomial
-                    H = (Yc * Yc') .* (((X * X') + 1) .^ param);
-                else           % guassian
-                    H = (Yc * Yc') .* grbf_fast(X,X,param);
-                end
+                % Create a classifier for each class
+                for c = unique(Y)'
+                    % Make true-classes: +1 and false-classes: -1
+                    Y_train_class = ((Y_train == c) * 2) - 1; 
+                    Y_test_class = ((Y_test == c) * 2) - 1; 
 
-                H = H + eye(l)*1e-7; % TODO: check if this is badly conditioned
-                P = -ones(size(Yc)); % negative because matlab by default minimizes quadprod, but we want a maximization
-                Aeq = Yc';
-                beq = 0;
-
-                % See 2.16c
-                lb = zeros(size(Yc));
-                ub = C * ones(size(Yc));
-
-                alpha = quadprog(H, P, [], [], Aeq, beq, lb, ub);
-%                 alpha = quadprog(H, P, [], [], [], [], lb, ub);
-
-                e = 1e-5;
-                ind_Free = find(alpha >= e & alpha <= C - e);
-                ind_Support_Vectors = find(alpha >= e);
-                X_free = X(ind_Free,:);
-                X_Support_Vectors = X(ind_Support_Vectors,:);
-                Y_Support_Vectors = Y(ind_Support_Vectors,:);
-
-                % Figure out the bias from the slideshow p.162/209
-                b = 0;
-                for j = ind_Free'
-                    sum = 0;
-                    for i = ind_Support_Vectors'
-                        if kernel == 1 % polynomial
-                            sum = sum + (((X(j,:) * X(i,:)') + 1) .^ param);
-                        else           % guassian
-                            sum = sum + grbf_fast(X(j,:),X(i,:),param);
-                        end
+                    if kernel == 1 % polynomial
+                        H = (Y_train_class * Y_train_class') .* (((X_train * X_train') + 1) .^ param);
+                    else           % guassian
+                        H = (Y_train_class * Y_train_class') .* grbf_fast(X_train,X_train,param);
                     end
-                    b = Y(j) + sum;
-                end
-                b = (1/length(ind_Free)) * b;
 
-                M = 1/norm(W);
+                    H = H + eye(l)*1e-7; % TODO: check if this is badly conditioned
+                    P = -ones(size(Y_train_class)); % negative because matlab by default minimizes quadprod, but we want a maximization
+                    Aeq = Y_train_class';
+                    beq = 0;
 
-                % Calculate Y_pred
-                Y_pred = zeros(size(Y));
-                for j = 1:l
-                    for i = ind_Support_Vectors'
-                        if kernel == 1 % polynomial
-                            Y_pred(j) = Y_pred(j) + (((X(j,:) * X(i,:)') + 1) .^ param);
-                        else           % guassian
-                            Y_pred(j) = Y_pred(j) + grbf_fast(X(j,:),X(i,:),param);
+                    % See 2.16c
+                    lb = zeros(size(Y_train_class));
+                    ub = C * ones(size(Y_train_class));
+
+                    alpha = quadprog(H, P, [], [], Aeq, beq, lb, ub);
+%                     alpha = quadprog(H, P, [], [], [], [], lb, ub);
+
+                    e = 1e-5;
+                    ind_Free = find(alpha >= e & alpha <= C - e);
+                    ind_Support_Vectors = find(alpha >= e);
+                    X_free = X_train(ind_Free,:);
+                    X_Support_Vectors = X_train(ind_Support_Vectors,:);
+                    Y_Support_Vectors = Y_train(ind_Support_Vectors,:);
+
+                    % Figure out the bias from the slideshow p.162/209
+                    b = 0;
+                    for j = ind_Free'
+                        sum = 0;
+                        for i = ind_Support_Vectors'
+                            if kernel == 1 % polynomial
+                                sum = sum + (((X_train(j,:) * X_train(i,:)') + 1) .^ param);
+                            else           % guassian
+                                sum = sum + grbf_fast(X_train(j,:),X_train(i,:),param);
+                            end
                         end
+                        b = Y_train_class(j) + sum;
                     end
-                    Y_pred(j) = Y_pred(j) + b;
+                    b = (1/length(ind_Free)) * b;
+
+                    M = 1/norm(W);
+
+                    % Calculate Y_pred for X_test
+                    Y_pred = zeros(size(Y_test));
+                    for j = 1:l_test
+                        for i = ind_Support_Vectors'
+                            if kernel == 1 % polynomial
+                                Y_pred(j) = Y_pred(j) + (((X_test(j,:) * X_train(i,:)') + 1) .^ param);
+                            else           % guassian
+                                Y_pred(j) = Y_pred(j) + grbf_fast(X_test(j,:),X_train(i,:),param);
+                            end
+                        end
+                        Y_pred(j) = Y_pred(j) + b;
+                    end
+                    Y_pred_all(:,c) = Y_pred';
                 end
-                Y_pred_all(:,c) = Y_pred';
+
+                [~, ind] = max(Y_pred_all, [], 2);
+                
+                numErr = numErr + length(find(ind-Y_test));
             end
 
-            [~, indices] = max(Y_pred_all'); length(Y); length(find(i'-Y));
-
+            totalErr = 100 * (numErr / length(Y));
+            
             % Store num_errors and percent_accuracy for plotting
             if kernel == 1 
-                err_polynomial(p_i) = length(find(indices'-Y));
-                accuracy_polynomial(p_i) = 1 - (err_polynomial(p_i) / length(Y));
+                accuracy_polynomial(p_i) = totalErr;
             else
-                err_guassian(p_i) = length(find(indices'-Y));
-                accuracy_guassian(p_i) = 1 - (err_guassian(p_i) / length(Y));
+                accuracy_guassian(p_i) = totalErr;
             end
         end
 
@@ -201,16 +225,12 @@ for kernel = 1:2 % 1: polynomial, 2: guassian % TODO: add guassian
                 labels_i = labels_i + 1;
 
                 figure(2);
-                plot(parameters_polynomial, err_polynomial);
-                figure(3);
                 plot(parameters_polynomial, accuracy_polynomial);
             else
                 label_strings(labels_i) = "Guassian, C=" + C;
                 labels_i = labels_i + 1;
 
-                figure(4);
-                plot(parameters_guassian, err_guassian);
-                figure(5);
+                figure(3);
                 plot(parameters_guassian, accuracy_guassian);
             end
     end
@@ -219,35 +239,28 @@ for kernel = 1:2 % 1: polynomial, 2: guassian % TODO: add guassian
     if kernel == 1 
         figure(2)
         legend(label_strings(1:length(label_strings)/2));
-        title("Number of Misclassified Points per Polynomial Parameter Used")
+        title("Percentage of Incorrectly Classified Elements through Crossvalidation")
         xlabel("Polynomial Parameter Used")
-        ylabel("Number of misclassified points")
-
-        figure(3)
-        legend(label_strings(1:length(label_strings)/2));
-        title("Percentage of Inputs Correctly Classified")
-        xlabel("Polynomial Parameter Used")
-        ylabel("Percentage Correctly Classified")
+        ylabel("Percentage Error")
     else
-        figure(4)
+        figure(3)
         legend(label_strings(length(label_strings)/2 + 1:length(label_strings)));
-        title("Number of Misclassified Points per Guassian Parameter Used")
+        title("Percentage of Incorrectly Classified Elements through Crossvalidation")
         xlabel("Guassian Parameter (Sigma) Used")
-        ylabel("Number of misclassified points")
+        ylabel("Percentage Error")
         set(gca, 'XScale', 'log')
-
-        figure(5)
-        legend(label_strings(length(label_strings)/2 + 1:length(label_strings)));
-        title("Percentage of Inputs Correctly Classified")
-        xlabel("Guassian Parameter (Sigma) Used")
-        ylabel("Percentage Correctly Classified")
-        set(gca, 'XScale', 'log')
-    end
-                
+    end      
 end
 
 hold off
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Create a model with the best parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% alpha = train_soft_margin_SVM()
+
+toc;
 
 %%%%%%%%%%%%%%
 % 
