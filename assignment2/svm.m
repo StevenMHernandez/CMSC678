828da46a6814f6c1e823c686d3a2c6886912f315
StevenMHernandez <contact@stevenhdesigns.com>
2018-10-22 17:27:01 -0400

    assignment2: remove comments for part 1

--- assignment2/svm.m
+++ assignment2/svm.m
@@ -7,82 +7,82 @@ options = optimset('maxIter',1e6,'LargeScale','off','Display','off');
 % suppress warnings when hessian matrix is not symetrical for quadprog
 warning('off','optim:quadprog:HessianNotSym')
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % 
-% % 1. Linear Hard-margin SVM
-% % 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 
-% load('P2_data.mat');
+% 1. Linear Hard-margin SVM
 % 
-% [l, dim] = size(X);
-% 
-% % See 2.16a from Springer book
-% H = (Y*Y').*(X*X');
-% H = H + eye(l)*1e-7;
-% P = -ones(size(Y)); % negative because matlab by default minimizes quadprod, but we want a maximization
-% % See 2.16b from Springer book
-% Aeq = Y';
-% beq = 0;
-% % See 2.16c
-% lb = zeros(size(Y));
-% ub = inf * ones(size(Y));
-% 
-% alpha = quadprog(H, P, [], [], Aeq, beq, lb, ub, [], options);
-% 
-% % Get weight
-% W = 0;
-% for i = 1:size(alpha,1)
-%     W = W + (alpha(i) * Y(i) * X(i,:)');
-% end
-% 
-% % Get bias
-% indices = (alpha > 1e-5); % only support vectors
-% b = 0;
-% X_sv = X(indices, :);
-% Y_sv = Y(indices);
-% l_sv = size(Y_sv, 1);
-% for i = 1:l_sv
-%     b = b + (1/Y_sv(i))-(X_sv(i,:)*W);
-% end
-% b = b / l_sv;
-% 
-% 
-% m = 1/norm(W);
-% 
-% "Part 1.a"
-% alphas_for_support_vectors = alpha(alpha > 1e-5)
-% b
-% M = m % As specified previously, scalar values should be lowercase, but the assignment states margin "M".
-% 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% % Find results for given datapoints
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 
-% "Part 1.b"
-% "o for [3 4]' = " + sign(W'*[3 4]'+b)
-% "o for [6 6]' = " + sign(W'*[6 6]'+b)
-% 
-% %%%%%%%%%%%%%%
-% % Plot Results
-% %%%%%%%%%%%%%%
-% 
-% figure(1);
-% hold on;
-% 
-% gscatter(X(:,1), X(:,2), Y, 'rb', '.+');
-% 
-% graph_line(W, b, m, 'b-', 'b--');
-% 
-% X_all = X(alpha > 1e-5, :);
-% plt = scatter(X_all(:,1), X_all(:,2), 'c', 'o');
-% 
-% grid on;
-% axis([-4 8 -4 8])
-% xlabel('x');
-% ylabel('y');
-% legend("-1", "+1", "decision hyperplane", "margin", "margin", "support vectors")
-% hold off;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load('P2_data.mat');
+
+[l, dim] = size(X);
+
+% See 2.16a from Springer book
+H = (Y*Y').*(X*X');
+H = H + eye(l)*1e-7;
+P = -ones(size(Y)); % negative because matlab by default minimizes quadprod, but we want a maximization
+% See 2.16b from Springer book
+Aeq = Y';
+beq = 0;
+% See 2.16c
+lb = zeros(size(Y));
+ub = inf * ones(size(Y));
+
+alpha = quadprog(H, P, [], [], Aeq, beq, lb, ub, [], options);
+
+% Get weight
+W = 0;
+for i = 1:size(alpha,1)
+    W = W + (alpha(i) * Y(i) * X(i,:)');
+end
+
+% Get bias
+indices = (alpha > 1e-5); % only support vectors
+b = 0;
+X_sv = X(indices, :);
+Y_sv = Y(indices);
+l_sv = size(Y_sv, 1);
+for i = 1:l_sv
+    b = b + (1/Y_sv(i))-(X_sv(i,:)*W);
+end
+b = b / l_sv;
+
+
+m = 1/norm(W);
+
+"Part 1.a"
+alphas_for_support_vectors = alpha(alpha > 1e-5)
+b
+M = m % As specified previously, scalar values should be lowercase, but the assignment states margin "M".
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Find results for given datapoints
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+"Part 1.b"
+"o for [3 4]' = " + sign(W'*[3 4]'+b)
+"o for [6 6]' = " + sign(W'*[6 6]'+b)
+
+%%%%%%%%%%%%%%
+% Plot Results
+%%%%%%%%%%%%%%
+
+figure(1);
+hold on;
+
+gscatter(X(:,1), X(:,2), Y, 'rb', '.+');
+
+graph_line(W, b, m, 'b-', 'b--');
+
+X_all = X(alpha > 1e-5, :);
+plt = scatter(X_all(:,1), X_all(:,2), 'c', 'o');
+
+grid on;
+axis([-4 8 -4 8])
+xlabel('x');
+ylabel('y');
+legend("-1", "+1", "decision hyperplane", "margin", "margin", "support vectors")
+hold off;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 
@@ -92,14 +92,14 @@ warning('off','optim:quadprog:HessianNotSym')
 
 % 1 - polynomial
 % 2 - gaussian
-kernel = 2;
+kernel = 1;
 
 [Y, X] = libsvmread('glass'); X=full(X);
 
 [l, dim] = size(X);
 
 % Scale each dimension
-for i = [1:dim]
+for i = 1:dim
     X(:,i) = (X(:,i) - mean(X(:,i))) / std(X(:,i));
 end
 
@@ -172,8 +172,6 @@ end
 [~, ind] = max(Y_pred_all, [], 1);
 ind = unique_classes(ind);
 numErr = length(find((ind - Y) ~= 0));
-
-percentErr = 100 * (numErr / length(Y));
 
 "3. Accuracy of SVM with best parameters:"
 accuracy = ((l - numErr) / l) * 100
